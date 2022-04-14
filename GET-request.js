2849fc9e15a7b27146edad6c0845e8a1bf42325d
rkadl9999 <rkadl9999@naver.com>
2022-04-14 10:06:52 +0900

    GET request

---
+++ GET-request.js
@@ -0,0 +1,35 @@
+//express 모듈 뷸러오기
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//Default Page
+app.get("/", (req, res) => {
+    res.send('This is default page');
+});
+
+//User Page
+app.get("/user", (req, res) => {
+    var name = req.param('name');   //파라미터로 받아와서 변수에 저장
+    var age = req.param('age');
+
+    if (name == null && age == null){           //파라미터 값 검사
+        res.send('Insert your name and age!');
+    }
+    else{
+        res.send('Hello '+name+' your age is '+age);        //이름과 나이 출력
+    }
+})
+
+/*------------------------------------------------------------------------------------------*/
+
+app.get("/Get/:name/:age", (req, res) => {  //바람직한 방법
+    var param = req.params; //변수에 객체 저장
+    console.log(param); //로그에 값 출력
+
+    res.send('user name : '+param.name+' / user age : '+param.age);
+});
+
+app.listen(port, () => {
+    console.log('Server is Running http://localhost:'+port);
+});
