abeff7eb84e3b0fdee89ee875afd5b8953d70f9e
rkadl9999 <rkadl9999@naver.com>
2022-04-16 22:15:59 +0900

    md5 해쉬값을 이용한 회원가입 구현

---
+++ sign-up.js
@@ -0,0 +1,33 @@
+//필요한 모듈 불러오기
+const express = require('express');
+const mysql = require('mysql');
+const db = require('./conn-DB-forMAC.js');
+const md5 = require('md5');
+const app = express();
+const port = 3000;
+
+app.use(express.json()); //데이터를 json 형식으로 받음
+app.use(express.urlencoded({ extended: false }));
+
+//삽입 SQL문 정의
+const insert = "insert into user values(?,?)";
+
+//Default Page
+app.get("/", (req, res) => {
+    res.send('This is Default Page');
+});
+
+//회원가입 Page
+app.post("/signUp", (req, res) => {
+    let id = req.body.id;
+    let password = md5(req.body.password);      //SQL Injection으로 DB가 공격당할 것을 대비해 md5 해쉬값으로 저장
+
+    db.connection.query(insert,[id,password], (err, result) => {
+        if (err) throw error;       //error 처리
+        console.log(result); //성공 값 출력
+    })
+})
+
+app.listen(port, () => {
+    console.log('Server is Running at http://localhost:' + port);
+});
