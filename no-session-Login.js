d4d45801e0aae0a37b91a76a5f993c91b76040e9
Hyeon_uk <80202054+rkadl9999@users.noreply.github.com>
2022-04-25 20:07:50 +0900

    Update no-session-Login.js

--- no-session-Login.js
+++ no-session-Login.js
@@ -9,6 +9,9 @@ const port = 3000;
 app.use(express.json()); //데이터를 json 형식으로 받음
 app.use(express.urlencoded({ extended: false }));
 
+//select 쿼리 문 작성
+let select = 'select * from user where id = ?';
+
 //Default Page
 app.get("/", (req, res) => {
     res.send('This is Default Page');
@@ -19,7 +22,7 @@ app.post("/login", (req, res) => {
     let id = req.body.id;
     let password = md5(req.body.password); //DB에 해쉬 처리하여 저장하였기 때문에 값 비교를 위해 해쉬 처리
     
-    db.connection.query('select * from user where id = ?', [id], (err, result) => {
+    db.connection.query(select, [id], (err, result) => {
         console.log(result); //쿼리 결과값 확인
         //로그인 로직
         if(id == result[0].id && password == result[0].password){
